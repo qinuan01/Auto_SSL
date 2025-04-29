59fd306f9c54e5e4fe8806d0a7b342fb8e2eb271
qinuan01 <142510302+qinuan01@users.noreply.github.com>
2025-04-29 23:02:01 +0800

    Update L.sh

--- L.sh
+++ L.sh
@@ -13,12 +13,12 @@ service nginx stop
 ./lego --email="admin@$domain" --domains="$domain" --http -a run
 service nginx start
 if ls ./.lego/certificates | grep "$domain"
-    then
-    mkdir -p /home/ssl/$domain
-    cp ./.lego/certificates/$domain.crt /home/ssl/$domain/1.pem
-    cp ./.lego/certificates/$domain.key /home/ssl/$domain/1.key
-    path="/home/ssl/$domain/"
-    echo '证书签发成功，证书文件保存在'$path'。'
+then
+  mkdir -p /home/ssl/$domain
+  cp ./.lego/certificates/$domain.crt /home/ssl/$domain/1.pem
+  cp ./.lego/certificates/$domain.key /home/ssl/$domain/1.key
+  path="/home/ssl/$domain/"
+  echo '证书签发成功，证书文件保存在'$path'。'
 else
-    echo '证书签发失败，请检查80端口是否被占用，域名解析或者输入域名是否正确。'
+  echo '证书签发失败，请检查80端口是否被占用，域名解析或者输入域名是否正确。'
 fi
